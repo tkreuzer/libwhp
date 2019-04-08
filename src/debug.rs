d2ac0cee861a0ae2d209d51f0bd25e794e9c20ee
Timo Kreuzer <timo@crowdstrike.com>
2019-04-08 17:59:08 +0200

    WIP: Improve dump_run_context

--- src/debug.rs
+++ src/debug.rs
@@ -3,6 +3,8 @@ pub use win_hv_platform_defs::*;
 pub use win_hv_platform_defs_internal::*;
 
 pub fn dump_run_context(run_context: &WHV_RUN_VP_EXIT_CONTEXT) {
+    println!("ExitReason: {:?}", run_context.ExitReason);
+    println!("Reserved = {}", run_context.Reserved);
     println!("Run context: {:?}", run_context.VpContext);
     println!("Execution state: {}", run_context.VpContext.ExecutionState);
 }
